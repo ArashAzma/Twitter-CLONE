68719a971be27338b592772f8fd594825d710c61
ArashAzma <arashazma3@gmail.com>
2023-08-16 22:43:54 +0330

    i dont know aymore

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['lh3.googleusercontent.com'],
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=1, stale-while-revalidate=59',
+          },
+        ],
       },
+    ];
+  },
+  images: {
+      domains: ['lh3.googleusercontent.com'],
+    },
 }
 
 module.exports = nextConfig
